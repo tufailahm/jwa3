5acad34f776a9bb462573d6cb150d3aecb9e4fc4
Tufail Ahmed <tufailahmedkhan@gmail.com>
2021-10-27 21:18:04 +0530

    final commit for spring testing

--- src/test/java/com/ReavtureSpringProductApplicationTests.java
+++ src/test/java/com/ReavtureSpringProductApplicationTests.java
@@ -6,7 +6,11 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
@@ -19,11 +23,13 @@ import org.springframework.web.client.RestTemplate;
 import com.training.model.Product;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@TestMethodOrder(value = MethodOrderer.OrderAnnotation.class)
 class ReavtureSpringProductApplicationTests extends AbstractTest {
 
 	@LocalServerPort
 	private String port;
 
+	int productId =0;
 	private String baseURL = "http://localhost";
 
 	URL restURL;
@@ -31,10 +37,13 @@ class ReavtureSpringProductApplicationTests extends AbstractTest {
 	@BeforeEach
 	public void setUp() throws MalformedURLException {
 		super.setUp();
+		productId = 899;
 		restURL = new URL(baseURL + ":" + port + "/product");
 	}
 
 	@Test
+	@Order(5)
+	@DisplayName("Testing product list all functionality")
 	void testGetProducts() throws Exception {
 
 		MvcResult mvcResult = mvc
@@ -47,15 +56,17 @@ class ReavtureSpringProductApplicationTests extends AbstractTest {
 		System.out.println("Product Content :  " + content);
 
 		Product[] productList = super.mapFromJson(content, Product[].class);
-		assertEquals(productList.length, 2);
+		assertEquals(productList.length, 1);
 
 	}
 
 	@Test
+	@Order(2)
+	@DisplayName("Testing product update functionality")
 	void testUpdateProduct() throws Exception {
 
 		Product product = new Product();
-		product.setProductId(43);
+		product.setProductId(productId);		//100
 		product.setProductName("Monitor");
 		product.setPrice(9800);
 		product.setQuantityOnHand(100);
@@ -75,14 +86,64 @@ class ReavtureSpringProductApplicationTests extends AbstractTest {
 
 	}
 
+	
+	@Test
+	@Order(3)
+	@DisplayName("Testing product get a single record functionality")
+	void testProduct() throws Exception {
+
+			//To do hands on
+	}
+	
+	
+	
+	//testing save
+	@Test
+	@Order(1)
+	@DisplayName("Testing product save functionality")
+	void testSaveProduct() throws Exception {
+
+		Product product = new Product();
+		product.setProductId(productId);		//100
+		product.setProductName("HPLaptop");
+		product.setPrice(9800);
+		product.setQuantityOnHand(100);
+
+		// convert the object into json
+		String inputJson = super.mapToJson(product);
+
+		MvcResult mvcResult = mvc
+				.perform(MockMvcRequestBuilders.post(restURL.toString()).contentType(MediaType.APPLICATION_JSON_VALUE)
+						.content(inputJson)).andReturn();
+
+		int status = mvcResult.getResponse().getStatus();
+		assertEquals(201, status);
+		String successMessage = "Product saved successfully :" + product.getProductId();
+		String content = mvcResult.getResponse().getContentAsString();
+		assertEquals(content, successMessage);
+
+	}
+	
+	
+	
 	//--Hands on -- test delete method
 	
 	@Test
+	@Order(4)
+	@DisplayName("Testing product delete functionality")
 	void testDeleteProduct() throws Exception {
-
-		String deleteURL = restURL.toString() + "/2";
+		//localhost:9090/product/2	- DELETE
+		String deleteURL = restURL.toString() + "/"+productId;		//100
 		
 		//code to test delete method
+		MvcResult mvcResult = mvc
+				.perform(MockMvcRequestBuilders.delete(deleteURL)).andReturn();
+
+		int status = mvcResult.getResponse().getStatus();
+		assertEquals(204, status);
+		String successMessage = "Product deleted successfully";
+		String content = mvcResult.getResponse().getContentAsString();
+		assertEquals(content, successMessage);
 
 	}
 	
